8e3a36dc1c69e917900d98ca9e766ae3e335e059
andatovi <andatovi@gmail.com>
2024-04-21 15:54:07 -0500

    Se envia la versión final del crud 1

---
+++ src/Logica/Conexion.java
@@ -0,0 +1,35 @@
+
+package Logica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class Conexion {
+    String bd = "easysoat";
+    String url = "jdbc:mysql://localhost:3306/";
+    String user = "root";
+    String pass = "Pollo0487";
+    String driver = "com.mysql.cj.jdbc.Driver";
+    Connection cx;
+
+    public Conexion() {
+    }
+    
+    public Connection conectar(){
+        try {
+            Class.forName(driver);
+            cx = DriverManager.getConnection(url+bd, user, pass);
+            
+            
+        } catch (ClassNotFoundException |SQLException ex) {
+            System.out.println("NO SE CONECTO A LA BASE DE DATOS");
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cx;
+    }
+}
